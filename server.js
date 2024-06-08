73a0e6bc66efa35a3760c35d606b63769d95a4ce
Glitch (glitch-hello-node) <none>
2024-06-08 17:49:30 +0000

    🏟🏏 Checkpoint ./server.js:60225415/1281

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
-import { WebSocketServer } from 'ws';
-
-const wss = new WebSocketServer({ port: 8080 });
-wss.on('connection', function connection(ws) {
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
-  });
-
-  ws.send('something');
+const WebSocket = require('ws');
+const wsServer = new WebSocket.Server({
+  port: process.env.PORT
 });
+
+wsServer.on('connection', function(socket){
+    // Some feedback on the console
+    console.log((new Date()) + " | Client connected");
+
+    // Attach some behavior to the incoming socket
+
+    socket.on('message', function(msg){
+        console.log((new Date()) + "| Received data from client | " + msg);
+        // socket.send("Take this back: " + msg);
+
+        // Brodcast that message to all connected clients
+        let countClients = 0;
+        wsServer.clients.forEach(function(client){
+            countClients = countClients+1
+            client.send(msg);
+        });
+
+    })
+
+});
+
+console.log("URL: " + "https://" + process.env.PROJECT_DOMAIN + ".glitch.me" + " | " + "Port:  " + process.env.PORT)
