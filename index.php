224b05a9daee8c78c89125d072df5a1f215dc46a
Glitch (glitch-hello-node) <none>
2024-06-12 13:54:32 +0000

    👘🍕 Checkpoint

--- index.php
+++ index.php
@@ -1,16 +1,8 @@
-<?php
-$servername = "cv9.h.filess.io";
-$username = "kappa_herfortywe";
-$password = "f9880c608fe34ac102a612d06f05235a7c801275";
-$count = json_decode(countClients, true);
-
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-?>
-<script src="server.js"></script>
+-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktdjEAAAAACmFlczI1Ni1jYmMAAAAGYmNyeXB0AAAAGAAAABAtuFimtu
+EPQlw2gH6DEaZRAAAAEAAAAAEAAAAzAAAAC3NzaC1lZDI1NTE5AAAAICEtoG3oAUWVew/u
+r9enje3txZ8yv81842HAsrvA8YC/AAAAoB/WybJ8r2Ki5dj9fS35opUi6qvpdCUol7BRBK
+7KioLr9rAu9G45ploLwN3hqwHAbh/TLycqmhlbrOvEx06YEoWvRr6cZGqQtu0Hnmi9g+bK
+fEqQuNvyx3N7hhrjZNIFBrsrqVgZ9u/mAgJI6fh7WjZq7CecKLm23CKdFltOGnTjBl8cM0
+fUGRdFeUYeIPOBZ8pY67S180V5UWClDFr4g+Q=
+-----END OPENSSH PRIVATE KEY-----
