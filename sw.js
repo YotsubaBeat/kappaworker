d5bd641b12beeafcf7358b52070c202b9e523dd0
Miyako Uehara <yotsubabeat@hotmail.com>
2024-06-17 08:03:16 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,6 +2,13 @@ importScripts('uv.sw.js');
 
 const sw = new UVServiceWorker();
 
+const cacheFiles = [
+  '/',
+  'public/index.html',
+  'server.js',
+  'public/style.css'
+];
+
 self.addEventListener('fetch', event =>
     event.respondWith(
         sw.fetch(event)
