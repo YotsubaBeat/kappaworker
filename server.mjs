0c3c6917b76f0dd94b333fc8a14aea07bf0390c1
Miyako Uehara <yotsubabeat@hotmail.com>
2024-06-18 15:15:59 +0000

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,7 +1,7 @@
 import express from 'express';
-import WebSocket from 'ws';
+import { WebSocketServer } from 'ws';
 // Declares itself as a websocket server
-const wsServer = new WebSocket.Server({ port: process.env.PORT });
+const wsServer = new WebSocketServer({ port: process.env.PORT });
 wsServer.on('connection', function(socket){
     // Logs client connection on connect
     console.log((new Date()) + " | Client connected");
