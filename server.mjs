9cf85657b4ace4cdf17e8c034e27ece39009d654
Miyako Uehara <yotsubabeat@hotmail.com>
2024-06-19 15:47:03 +0000

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,32 +1,23 @@
-import express from 'express';
-import { WebSocketServer } from 'ws';
-import * as swRegister from 'register-service-worker';
-swRegister.register('sw.cjs', {
-  registrationOptions: { 
-      scope: __uv$config.prefix 
-  }
-});
-// Declares itself as a websocket server
-const wsServer = new WebSocketServer({ port: process.env.PORT });
-wsServer.on('connection', function(socket){
-    // Logs client connection on connect
-    console.log((new Date()) + " | Client connected");
-    socket.on('message', function(msg){
-        // Logs input received from client
-        console.log((new Date()) + " | Received input from client: " + "[" + msg + "]");
-        // Converts input to a working URL
-        if (!isUrl(msg)) {
-          msg = 'https://www.google.com/search?q=' + msg;
-        } else if (isUrl(msg)) { 
-          msg = 'https://' + msg;
-        }
-        function isUrl(val = val.trim()) {
-          if (/^http(s?):\/\//.test(val) || val.includes('.')) 
-            return true || false;
-        };
-        // Responds to websocket with a encoded URL of input
-        socket.send(__uv$config.prefix + __uv$config.encodeUrl(msg));
-    })
-});
-// Logs websocket status
-console.log((new Date()) + " | Websocket Server opened on port: " + process.env.PORT)
+const http = require('http');
+ const fs = require('fs');
+
+ const host = 'https://serviceworker.cleverapps.io';
+ const port = process.env.PORT;
+
+ const httpServer = http.createServer(httpHandler);
+
+ httpServer.listen(port, host, () => {
+     console.log(`HTTP server running at http://${host}:${port}/`);
+ });
+
+ function httpHandler(req, res) {
+     fs.readFile(req.url, function (err, data) {
+
+         if (err == null ) {
+
+             res.writeHead(200, {'Content-Type': 'text/html'});
+             res.write(data);
+             res.end();
+         }
+     });
+ }
