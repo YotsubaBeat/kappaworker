cd9ea50063f4b66b57a8505c25e86db532b1b6c5
Miyako Uehara <yotsubabeat@hotmail.com>
2024-06-20 15:51:25 +0000

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
  const fs = require('fs');
 
  const port = process.env.PORT;
- const host = process.env.APP_ID + ".cleverapp.io";
+ const host = localhost;
 
  const httpServer = http.createServer(httpHandler);
 
