549f9e48010ac937795324e71553e4c4cf3e60d0
Miyako Uehara <yotsubabeat@hotmail.com>
2024-06-20 15:13:15 +0000

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const http = require('http');
  const fs = require('fs');
 
  const port = process.env.PORT;
+ const host = 'https://app-8bea6ced-fb1b-436e-889a-38337f578636.cleverapps.io/';
 
  const httpServer = http.createServer(httpHandler);
 
- httpServer.listen(port, () => {
+ httpServer.listen(port, host, () => {
      console.log((new Date()) + ' | HTTP Server running on port: ' + port);
  });
 
