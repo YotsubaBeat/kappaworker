f9b9a04ba8fd21521ad53acefc8f36310ef8679f
Miyako Uehara <yotsubabeat@hotmail.com>
2024-06-26 13:38:49 +0000

    Update respond.mjs

--- respond.mjs
+++ respond.mjs
@@ -1,1 +1,10 @@
-
+import * as input from '/server.js';
+import * as swRegister from 'register-service-worker';
+swRegister.register('/sw.cjs',{
+    registrationOptions: {
+        scope: __uv$config.prefix
+    }
+});
+encode = __uv$config.prefix + __uv$config.encodeUrl(input.respond)); 
+data["response"] = encode;
+module.exports = data;
