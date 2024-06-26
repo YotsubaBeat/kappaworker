44e14fab7edbf541e24e13c91a20997178e268e8
Miyako Uehara <yotsubabeat@hotmail.com>
2024-06-26 13:28:15 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const encodedURL = require('/respond.cjs')
 const WebSocket = require('ws');
 const http = require('http');
 const express = require('express');
@@ -13,3 +14,25 @@ const port = process.env.PORT;
 const wsServer = new WebSocket.Server({ server: httpserver });
 httpserver.listen(port);
 console.debug('Server listening on port ' + port);
+wsServer.on('connection', function(socket){
+    // Logs client connection on connect
+    console.log((new Date()) + " | Client connected");
+    socket.on('message', function(msg){
+        // Logs input received from client
+        console.log((new Date()) + " | Received input from client: " + "[" + msg + "]");
+        // Converts input to a working URL
+        const encodedWebSocket = 0;
+        if (!isUrl(msg)) {
+          encodedWebSocket = 'https://www.google.com/search?q=' + msg;
+        } else if (isUrl(msg)) { 
+          encodedWebSocket = 'https://' + msg;
+        }
+        function isUrl(val = val.trim()) {
+          if (/^http(s?):\/\//.test(val) || val.includes('.')) 
+            return true || false;
+        };
+        // Responds to websocket with a encoded URL of input
+        data["request"] = encodedWebSocket;
+        module.exports = data;
+    })
+});
