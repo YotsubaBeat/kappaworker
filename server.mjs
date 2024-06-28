24d0471b3b2cb13406d3952d086d5f79fc76ee91
Miyako Uehara <yotsubabeat@hotmail.com>
2024-06-28 18:04:43 +0200

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -10,8 +10,9 @@ const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 const app = express();
 const port = process.env.PORT;
 const wsServer = new WebSocketServer({ server: app });
-app.use(sphp.express(__dirname+"'"+process.env.HTTP+"'"));
-app.use(express.static(__dirname+"'"+process.env.HTTP+"'"));
+app.use("/", php.cgi(p, { path.join(process.env.HTTP) }));
+app.use("/phpdynamic", php.cgi( __dirname+process.env.HTTP));
+app.listen(process.env.PORT,"localhost");
 wsServer.on('connection', function(socket) {
     // Logs client connection on connect
     console.log((new Date()) + " | Client connected");
