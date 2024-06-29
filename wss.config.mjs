6ceef50522469a4ce4d2e20e900f1923fd611acb
Miyako Uehara <yotsubabeat@hotmail.com>
2024-06-29 16:00:53 +0000

    Update wss.config.mjs

--- wss.config.mjs
+++ wss.config.mjs
@@ -4,4 +4,4 @@ const PORT = 8080;
 // The file path to the PHP document that is runned by the HTTP server
 const HTTP = "/consepts/default.html";
 
-module.exports = { HTTP, PORT }
+export { HTTP, PORT }
