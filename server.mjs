7c404a2107213ecf9fa398ca75f205deb14d7716
Miyako Uehara <yotsubabeat@hotmail.com>
2024-06-30 14:44:25 +0000

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -5,7 +5,7 @@ import express from 'express';
 import * as path from 'path';
 import * as url from 'url';
 import * as fs from 'fs';
-import * as expressWS from 'express-ws';
+import { expressWs } from 'express-ws';
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 const app = express();
 const filePath = path.join(__dirname+config.HTTP);
@@ -15,7 +15,7 @@ app.get('/', function(req, res) {
 app.listen(config.PORT, () => {
   console.log((new Date())+" | Server is listening on port "+config.PORT)
 })
-app.ws('/', function(ws, req) {
+expressWs('/', function(ws, req) {
     ws.on('message', function(msg){
         // Logs input received from client
         console.log((new Date())+" | Received input from client: ["+msg+"]");
