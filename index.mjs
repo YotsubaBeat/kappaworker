e2ca0626022f036997a821e3ac82ed71c851027d
Miyako Uehara <yotsubabeat@hotmail.com>
2024-06-30 16:14:39 +0000

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,32 +1,9 @@
-import * as swRegister from 'register-service-worker';
-import * as WebSocket from 'ws';
-swRegister.register('/sw.cjs',{
+  import * as swRegister from './register-service-worker';
+  import * as input from './server.cjs';
+swRegister.register('./sw.cjs',{
     registrationOptions: {
         scope: __uv$config.prefix
     }
 });
-// Declares itself as a websocket server
-const wsServer = new WebSocket.Server({ port: process.env.PORT });
-wsServer.on('connection', function(socket){
-    // Logs client connection on connect
-    console.log((new Date()) + " | Client connected");
-    socket.on('message', function(msg){
-        // Logs input received from client
-        console.log((new Date()) + " | Received input from client: " + "[" + msg + "]");
-        // Converts input to a working URL
-        const encodedWebSocket = 0;
-        if (!isUrl(msg)) {
-          encodedWebSocket = 'https://www.google.com/search?q=' + msg;
-        } else if (isUrl(msg)) { 
-          encodedWebSocket = 'https://' + msg;
-        }
-        function isUrl(val = val.trim()) {
-          if (/^http(s?):\/\//.test(val) || val.includes('.')) 
-            return true || false;
-        };
-        // Responds to websocket with a encoded URL of input
-        socket.send(__uv$config.prefix + __uv$config.encodeUrl(encodedWebSocket));
-    })
-});
-// Logs websocket status
-console.log((new Date()) + " | Websocket Server opened on port: " + process.env.PORT);
+encode = __uv$config.prefix + __uv$config.encodeUrl(input.encodedWebSocket); 
+module.exports = encode;
