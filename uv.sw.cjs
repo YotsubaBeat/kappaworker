de7a0f415df083a8c759c9ec6eb5ba7fb734a30d
Miyako Uehara <yotsubabeat@hotmail.com>
2024-07-01 19:10:23 +0000

    Update uv.sw.cjs

--- uv.sw.cjs
+++ uv.sw.cjs
@@ -1,18 +1,5 @@
 importScripts('uv.bundle.cjs');
-var self = this;
-self.__uv$config = {
-    // The service prefix of the encoded webpages
-    prefix: '/service/',
-    // The full URL of the bare server
-    bare: 'https://aluu.xyz/bare/',
-    encodeUrl: Ultraviolet.codec.xor.encode,
-    decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: 'uv.handler.cjs',
-    bundle: 'uv.bundle.cjs',
-    config: 'uv.config.cjs',
-    sw: 'uv.sw.cjs'
-};
-module.exports.__uv$config = __uv$config;
+importScripts('uv.config.cjs');
 
 class UVServiceWorker extends EventEmitter {     
     constructor(config = __uv$config) {
