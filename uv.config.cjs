6b51b3a522c8f2b708f76eaacd95672a1ab3b533
Miyako Uehara <yotsubabeat@hotmail.com>
2024-07-01 19:17:06 +0000

    Update uv.config.cjs

--- uv.config.cjs
+++ uv.config.cjs
@@ -1,7 +1,5 @@
 var self = this;
-import('uv.bundle.cjs').then(({ Ultraviolet }) => {
-    const Ultraviolet = Ultraviolet;
-});
+import('uv.bundle.cjs');
 self.__uv$config = {
     // The service prefix of the encoded webpages
     prefix: '/service/',
