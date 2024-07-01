5fdf3241fd3133ed1f5a6090a628b3da5aad654c
Miyako Uehara <yotsubabeat@hotmail.com>
2024-07-01 10:05:37 +0000

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -23,23 +23,24 @@ import('./wss.config.mjs').then(({ HTTP, PORT }) => {
     ws.on('message', function(msg) {
         // Logs input received from client
         console.log((new Date())+" | Received input from client: ["+msg+"]");
-        // Converts input to a working URL
-        // Exports the encoded websocket as a module
-        // Sends the final product back to the client once it has finished
+        // Registers the service worker
         global.navigator.serviceWorker.register('./sw.cjs',{ 
           scope: __uv$config.prefix
         });
+        // Converts input to a working URL
         if (!isUrl(msg)) {
           let url = 'https://www.google.com/search?q=' + msg;
         } else if (isUrl(msg)) { 
           let url = 'https://' + msg;
-        }
+        };
         function isUrl(val = val.trim()) {
           if (/^http(s?):\/\//.test(val) || val.includes('.')) 
             return true || false;
-};
-          encode = __uv$config.prefix + __uv$config.encodeUrl(url); 
-          ws.send(encode);
+        };
+        // Encodes the websocket
+        encode = __uv$config.prefix + __uv$config.encodeUrl(url); 
+        // Sends the encoded websocket
+        ws.send(encode);
     });
   });
 });
